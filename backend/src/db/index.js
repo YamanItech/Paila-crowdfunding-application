ca56957cd7afb2de32e8427fb9c9380284493cf4
yamaitech <eishworacharya@gmail.com>
2025-02-22 02:52:15 +0545

    file configuration

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -10,3 +10,4 @@ const connectDB = async () => {
         process.exit(1);
     }
 }
+export default connectDB;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,18 @@
 import dotenv from "dotenv";
 import {app} from "./app.js";
+import connectDB from "./db/index.js";
 dotenv.config({
   path: "src/.env"
 });
 const port = process.env.PORT || 8001;
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port} `);
-});
+connectDB()
+    .then(() => {
+        app.listen(port,()=>{
+            console.log(`Server is running on port ${port} `);
+        });
+
+    })
+    .catch(err => {
+        console.log("MongoDB connection error:", err);
+    });
+
